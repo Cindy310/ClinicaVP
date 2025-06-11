ee839559731c565c346d22324a596425cbca9791
cindy hernandez <hr25001@esfe.agape.edu.sv>
2025-06-11 16:03:22 -0600

    Se realizo el Loginform

---
+++ src/main/java/esfe/presentacion/LoginForm.java
@@ -0,0 +1,11 @@
+package esfe.presentacion;
+
+import javax.swing.*;
+
+public class LoginForm extends JDialog {
+    private JPanel mainPanel;
+    private JTextField txtEmail;
+    private JPasswordField txtPassword;
+    private JButton btnLogin;
+    private JButton btnSalir;
+}
